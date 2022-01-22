93784ecc51f99dfcc95070d71857e5bafaa6fea3
carlos <carlosramos@sime.es>
2022-01-22 20:46:04 +0100

    styling component

--- modules/buttons.js
+++ modules/buttons.js
@@ -1,12 +1,48 @@
-export class GvButton extends HTMLElement {
+export class GravityButton extends HTMLElement {
+
+    text;
 
     constructor() {
+        console.log('GyButton charged');
         super();
         this.shadow = this.attachShadow({ mode: 'open' });
     }
 
-    attributeChangedCallback(property, oldValue, newValue) {
+    static get observedAttributes() {
+        return ['text', 'gy-style'];
+    }
 
+    attributeChangedCallback(prop, oldValue, newValue) {
+        console.log(prop);
+        switch(prop) {
+            case 'text': 
+                this.text = newValue;
+                this.render();
+                break;
+            case 'gy-style':
+                this.render();
+                const styleObject = JSON.parse(newValue);
+                this.setStyle(styleObject);
+                break;
+        }
+    }
+
+    setStyle(styleObject) {
+        const gyButton = this.shadow.querySelector('#gy-button');
+        if (styleObject) {
+            Object.keys(styleObject).forEach(style => {
+                console.log(styleObject[style]);
+            })
+        }
+    }
+
+    click() {
+        console.log('click');
+    }
+    render() {
+        this.shadow.innerHTML = `
+            <button id="gy-button">${ this.text }</button>
+        `
     }
 }
-customElements.define('gv-button', GvButton);
+customElements.define('gy-button', GravityButton);
--- modules/main.modules.js
+++ modules/main.modules.js
@@ -1,2 +1,3 @@
 import { HolaMundo } from './holaMundo.js';
-import { MyCounter } from './counter.js'
+import { MyCounter } from './counter.js';
+import { GravityButton } from './buttons.js';
