65638c5bcf1b537b89a4001a490519abf86a8f5e
carlos <carlosramos@sime.es>
2022-01-23 02:22:33 +0100

    refactor code to get template

--- components/gy-button/gy-button.js
+++ components/gy-button/gy-button.js
@@ -1,88 +1,90 @@
-const template = document.createElement('template');
-const DEFAULT_CSS_CLASS = 'default';
+const template = document.createElement("template");
+const DEFAULT_CSS_CLASS = "default";
+
 export class GYButton extends HTMLElement {
-    buttonTemplate = `
-        <style>
-            button {
-                padding: 5px; 
-                border-radius: 5px; 
-                cursor: pointer;
-                font-size: 1.2rem
-            }
-            .default {
-                background-color: black;
-                color: white;
-            }
-            .blue {
-                background-color: blue;
-                color: white;
-            }
-            .magenta {
-                background-color: magenta;
-                color: white;
-            }
-        </style>
-        <button 
-            id="gy-button" 
-            class="btn">
-            ${ this.text ?? 'Button' }
-        </button>
-    `;
+  
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    console.log("%c Component: GYButton", "color: #bada55");
+  }
 
-    constructor() {
-        console.log('%c Component: GYButton', 'color: #bada55');
-        super();
-        this.shadow = this.attachShadow({ mode: 'open' });
+  static get observedAttributes() {
+    return ["text", "type"];
+  }
+
+  get text() {
+    return this.getAttribute("text");
+  }
+
+  set text(value) {
+    this.setAttribute("text", value);
+  }
+
+  get type() {
+    return this.getAttribute("type");
+  }
+
+  set type(value) {
+    return this.setAttribute("type", value);
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(prop, oldValue, newValue) {
+    switch (prop) {
+      case "text":
+        break;
+      case "type":
+        break;
     }
+  }
 
-    static get observedAttributes() {
-        return ['text', 'type'];
-    }
+  emmitEvent() {
+    const messageEvent = new CustomEvent("message", {
+      bubbles: true,
+    });
+    this.btn.dispatchEvent(messageEvent);
+  }
 
-    get text() {
-        return this.getAttribute('text');
-    }
+  render() {
+    template.innerHTML = this.getTemplate();
+    this.shadow.appendChild(template.content.cloneNode(true));
+    this.btn = this.shadow.querySelector("#gy-button");
+    this.btn.addEventListener("click", this.emmitEvent.bind(this));
+    this.btn.className = this.type ?? DEFAULT_CSS_CLASS;
+  }
 
-    set text(value) {
-        this.setAttribute('text', value);
-    }
-
-    get type() {
-        return this.getAttribute('type');
-    }
-
-    set type(value) {
-        return this.setAttribute('type', value);
-    }
-
-    connectedCallback() {
-        this.render();
-    }
-
-    attributeChangedCallback(prop, oldValue, newValue) {
-        switch(prop) {
-            case 'text': 
-
-                break;
-            case 'type':
-                
-                break;
-        }
-    }
-
-    emmitEvent() {
-        const messageEvent = new CustomEvent('message', {
-            bubbles: true
-        });
-        this.btn.dispatchEvent(messageEvent);
-    }
-
-    render() {
-        template.innerHTML = this.buttonTemplate;
-        this.shadow.appendChild(template.content.cloneNode(true));
-        this.btn = this.shadow.querySelector('#gy-button');
-        this.btn.addEventListener('click', this.emmitEvent.bind(this));
-        this.btn.className = this.type ?? DEFAULT_CSS_CLASS;
-    }
+  getTemplate() {
+    return `
+      <style>
+          button {
+            padding: 5px; 
+            border-radius: 5px; 
+            cursor: pointer;
+            font-size: 1.2rem
+          }
+          .default {
+            background-color: black;
+            color: white;
+          }
+          .blue {
+            background-color: blue;
+            color: white;
+          }
+          .magenta {
+            background-color: magenta;
+            color: white;
+          }
+      </style>
+      <button 
+        id="gy-button" 
+        class="btn">
+        ${this.text ?? "Button"}
+      </button>
+    `
+  }
 }
-customElements.define('gy-button', GYButton);
+customElements.define("gy-button", GYButton);
